b0d387f619ab63a43df50033fd7115fa0b0869d2
sebastiandrozd97 <sebastian.drozd97@gmail.com>
2021-01-03 18:34:39 +0100

    feature: add route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,7 @@ import Notifications from '@/views/Notifications/Notifications.vue';
 import EmployeesDiary from '@/views/EmployeesDiary/EmployeesDiary.vue';
 import WorkplacesDiary from '@/views/WorkplacesDiary/WorkplacesDiary.vue';
 import Auth from '@/views/Auth/Auth.vue';
+import axios from 'axios';
 
 Vue.use(VueRouter);
 
@@ -16,42 +17,66 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/employees',
     name: 'Employees',
-    component: Employees
+    component: Employees,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/employees/:id',
     name: 'EmployeesDiary',
-    component: EmployeesDiary
+    component: EmployeesDiary,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/workplaces',
     name: 'Workplaces',
-    component: Workplaces
+    component: Workplaces,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/workplaces/:id',
     name: 'WorkplacesDiary',
-    component: WorkplacesDiary
+    component: WorkplacesDiary,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/clients',
     name: 'Clients',
-    component: Clients
+    component: Clients,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/notifications',
     name: 'Notifications',
-    component: Notifications
+    component: Notifications,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/settings',
     name: 'Settings',
-    component: Settings
+    component: Settings,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/auth',
@@ -64,6 +89,37 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach(async (to, from, next) => {
+  let isLoggedIn = false;
+  const request = await axios.get(`${process.env.VUE_APP_API_URL}/users/current`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem('accessToken')}` }
+  });
+
+  if (request.status === 200) {
+    isLoggedIn = true;
+  }
+
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    if (isLoggedIn) {
+      next();
+    } else {
+      next({
+        name: 'Auth'
+      });
+    }
+  } else if (to.matched.some(rec => rec.path === '')) {
+    if (isLoggedIn) {
+      next({
+        name: 'Home'
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
